b2bbbb24da4a1155a0c9fb1a9aa5b947ba9d9933
Thomas Colin de Verdière <tdeverdiere@iobeya.com>
2022-05-15 23:19:22 +0200

    Add Loader class that load model content and add dart:async folder

---
+++ new_dart_model.spec
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="ASCII"?>
+<xmi:XMI xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:dart="http://www.obeo.fr/dsl/2014/dart">
+  <dart:Project name="Dart">
+    <packages name="Dart Standard Library">
+      <assets xsi:type="dart:Folder" name="dart:collection">
+        <assets xsi:type="dart:Library" name="collection.dart">
+          <imports dartResource="/0/@packages.0/@assets.3/@assets.0"/>
+        </assets>
+      </assets>
+      <assets xsi:type="dart:Folder" name="dart:convert">
+        <assets xsi:type="dart:Library" name="convert.dart"/>
+      </assets>
+      <assets xsi:type="dart:Folder" name="dart:core">
+        <assets xsi:type="dart:Library" name="core.dart" parts="/0/@packages.0/@assets.2/@assets.1 /0/@packages.0/@assets.2/@assets.7 /0/@packages.0/@assets.2/@assets.4 /0/@packages.0/@assets.2/@assets.5 /0/@packages.0/@assets.2/@assets.6 /0/@packages.0/@assets.2/@assets.2 /0/@packages.0/@assets.2/@assets.8">
+          <imports dartResource="/0/@packages.0/@assets.0/@assets.0"/>
+          <imports dartResource="/0/@packages.0/@assets.1/@assets.0"/>
+          <imports dartResource="/0/@packages.0/@assets.3/@assets.0"/>
+        </assets>
+        <assets xsi:type="dart:Class" name="bool" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Class" name="double" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Class" name="Function"/>
+        <assets xsi:type="dart:Class" name="int" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Class" name="List" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Class" name="map" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Class" name="Object" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Class" name="String" partOf="/0/@packages.0/@assets.2/@assets.0"/>
+        <assets xsi:type="dart:Metadata" name="deprecated"/>
+        <assets xsi:type="dart:Metadata" name="override"/>
+        <assets xsi:type="dart:Metadata" name="proxy"/>
+      </assets>
+      <assets xsi:type="dart:Folder" name="dart:math">
+        <assets xsi:type="dart:Library" name="math.dart"/>
+      </assets>
+    </packages>
+  </dart:Project>
+  <dart:Folder name="dart:async"/>
+</xmi:XMI>
---
+++ src/main/java/model/Loader.java
@@ -0,0 +1,107 @@
+package model;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EFactory;
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Loader {
+
+    public static void main(String[] args) throws IOException {
+        XMIResource ecoreResource = new XMIResourceImpl();
+        try (var ecoreInputStream = Loader.class.getResourceAsStream("/dart.ecore")) {
+            ecoreResource.load(ecoreInputStream, null);
+        }
+
+        //logAllContent(ecoreResource);
+        EPackage ecorePackage = getPackage(ecoreResource);
+
+        ResourceSet resourceSet = new ResourceSetImpl();
+        resourceSet.getPackageRegistry().put(ecorePackage.getNsURI(), ecorePackage);
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+
+        Resource resource = resourceSet.createResource(URI.createURI("/dartlang.dartspec"));
+
+        try (var instanceInputStream = Loader.class.getResourceAsStream("/dartlang.dartspec")) {
+            resource.load(instanceInputStream, null);
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("List model content:");
+        logAllContent(resource);
+
+        // Add content
+        EFactory factoryInstance = ecorePackage.getEFactoryInstance();
+        EClass folderClass = (EClass) ecorePackage.getEClassifier("Folder");
+        EObject folderObject = factoryInstance.create(folderClass);
+        EAttribute folderName = folderClass.getEAllAttributes().get(0);
+        folderObject.eSet(folderName, "dart:async");
+
+        resource.getContents().add(folderObject);
+
+        String userDir = System.getProperty("user.dir");
+        File outputFile = Paths.get(userDir).resolve("new_dart_model.spec").toFile();
+
+        try (var outputStream = new FileOutputStream(outputFile)) {
+            Map options = new HashMap();
+            options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.FALSE);
+            try {
+                resource.save(outputStream, options);
+            } catch (IOException e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static void logAllContent(Resource resource) {
+        log("", resource.getContents());
+    }
+
+    static <T extends EObject> void log(String indent, EList<T> eObjects) {
+        eObjects.forEach(eObject -> log(indent, eObject));
+    }
+
+    static <T extends EObject> void log(String indent, T eObject) {
+        String content = indent + eObject.eClass().getName();
+        if (eObject instanceof ENamedElement eNamedElement) {
+            content = content + " " + eNamedElement.getName();
+        } else if (eObject instanceof DynamicEObjectImpl dynamicEObject) {
+            var name = dynamicEObject.dynamicGet(EcorePackage.EATTRIBUTE__NAME);
+            if (name != null) {
+                content = content + " " + name;
+            }
+        }
+
+        System.out.println(content);
+        var contents = eObject.eContents();
+        if (contents != null) {
+            log(indent + "  ", contents);
+        }
+    }
+
+    static EPackage getPackage(Resource res) {
+        return (EPackage)res.getContents().get(0);
+    }
+}
